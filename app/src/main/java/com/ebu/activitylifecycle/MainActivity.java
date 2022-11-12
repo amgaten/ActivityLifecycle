6c44036b107f01722313f6fbb8a7e35888023b4b
amgaten <amgaten@hotmail.com>
2022-11-11 20:38:30 -0500

    Example to activity lifecycle, 2nd upload

--- app/src/main/java/com/ebu/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/ebu/activitylifecycle/MainActivity.java
@@ -3,6 +3,7 @@ package com.ebu.activitylifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
+        // La actividad ya esta creada/ Activity is already created
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show();
+        // La actividad está a punto de hacerse visible. / Activity is about to become visible
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "OnResume", Toast.LENGTH_SHORT).show();
+        // La actividad se ha vuelto visible (ahora se "reanuda"). The activity has become visible (it is now "resumed")
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "OnPause", Toast.LENGTH_SHORT).show();
+        // Enfocarse en otra actividad  (esta actividad está a punto de ser "detenida"). / Focus on another activity (this activity is about to be "stopped")
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "OnStop", Toast.LENGTH_SHORT).show();
+        // La actividad ya no es visible (ahora está "detenida") / The activity is no longer visible (it is now "stopped")
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "OnDestroy", Toast.LENGTH_SHORT).show();
+        // La actividad está a punto de ser destruida. / Activity is about to be destroyed
     }
 }
